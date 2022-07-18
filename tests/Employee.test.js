e63c0e50abd9d44bbfc8f313a2cfba92bfd7b653
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-18 07:55:40 -0400

    skeleton of first employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,11 @@
 const Employee = require ('../employee')
+
+describe ("Employee", () => {
+    describe ("Initialization", () => {
+        it("should start the application to render the webpage", () => {
+            const obj = new Employee()
+            // still need to fill this in 
+            expect ("string" in obj).toEqual()
+        })
+    })
+})
